1eabe3aa4f0782c5173530bb15b74a596ceb4ba4
felipeB92 <felipe_B92@hotmail.com>
2026-05-02 18:13:16 -0300

    Backup Automatico - 02/05/2026

--- sync_github.bat
+++ sync_github.bat
@@ -1,10 +1,7 @@
 @echo off
 d:
-:: Agora o comando CD entra direto na pasta DATA
 cd "D:\Felipe\OB\Streets_Of_Rage_ Legacy1.5(2026)\DATA"
-
 git add .
-git commit -m "Backup Scripts OpenBOR - %date% %time%"
+git commit -m "Backup Automatico - %date%"
 git push origin main
-
 exit
