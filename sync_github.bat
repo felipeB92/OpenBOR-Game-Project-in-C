6b3072e354722b74d84a64b4718220ebe65b8ffc
felipeB92 <felipe_B92@hotmail.com>
2026-05-03 20:30:27 -0300

    Backup Automatico - 03/05/2026

--- sync_github.bat
+++ sync_github.bat
@@ -1,7 +1,14 @@
 @echo off
 d:
 cd "D:\Felipe\OB\Streets_Of_Rage_ Legacy1.5(2026)\DATA"
+
+:: Adiciona as alterações
 git add .
+
+:: Cria o commit com a data
 git commit -m "Backup Automatico - %date%"
-git push origin main
+
+:: O segredo está aqui: envia da master local para a main do GitHub
+git push origin master:main --force
+
 exit
